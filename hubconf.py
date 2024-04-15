7d802105e2489577e977ae50d8a7ae235d293279
Santhosi03 <110807524+Santhosi03@users.noreply.github.com>
2024-04-15 17:16:12 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,3 +1,13 @@
 def print_new():
   print("print_new")
   return "hubconf"
+
+import torchvision.models as models
+
+model = models.vgg16(weights='IMAGENET1K_V1')
+torch.save(model.state_dict(), 'model_weights.pth')
+
+
+def get_model():
+  print("hi")
+  return model
