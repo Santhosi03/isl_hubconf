eee0a84c7fe9190e47b1a2ff547d3d5b792fefc1
Santhosi03 <110807524+Santhosi03@users.noreply.github.com>
2024-04-26 21:38:23 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -4,6 +4,7 @@ def print_new():
   return "hubconf"
 
 import torchvision.models as models
+from sklearn.datasets import make_blobs
 
 model = models.vgg16(weights='IMAGENET1K_V1')
 torch.save(model.state_dict(), 'model_weights.pth')
@@ -12,3 +13,8 @@ torch.save(model.state_dict(), 'model_weights.pth')
 def get_model():
   print("model")
   return model
+
+def get_data_blobs(n_points=100):
+  X,y=make_blobs(n_samples=10,centers=2,n_features=2)
+  print(X.shape)
+  return X,y
